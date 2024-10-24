f81c7c6c08040f07b8ee71c0eb1d19930f20f39f
Anass-Zar <anasszarioh35@gmail.com>
2024-10-24 17:01:27 +0100

    ...

--- app/api/dashboard/your-stock/route.ts
+++ app/api/dashboard/your-stock/route.ts
@@ -3,15 +3,15 @@ import { db } from '@/lib/db';
 
 export const GET = async () => {
   try {
-    const totalStock = await db.product.aggregate({
-      _sum: {
-        stock: true,
+    const totalStock = await db.product.count({
+      where: {
+        stock: { gt: 0 },
       },
     });
     console.log(totalStock);
     return NextResponse.json({
       success: true,
-      totalStock: totalStock._sum.stock || 0,
+      totalStock: totalStock || 0,
     });
   } catch (error) {
     console.error('Error fetching total stock:', error);
